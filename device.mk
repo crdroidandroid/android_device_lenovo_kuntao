aaf0e24779c30ffe23faf5e9999a45345b0e2b24
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-10-31 20:40:48 +0200

    kuntao: Add audio_amplifier HAL
    * Used to turn on TAS2555 speaker amp
    * HAL code based on analyzing of stock audio.primary.msm8953
    
    Change-Id: Ifa8b5182b3cbb1a81df261d9fccceeb39a72100d
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

--- device.mk
+++ device.mk
@@ -62,6 +62,7 @@ PRODUCT_PACKAGES += \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8953 \
+    audio_amplifier.msm8953 \
     audio.usb.default \
     audio.r_submix.default \
     libaudio-resampler
