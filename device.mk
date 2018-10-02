e391a8463386a0de316f1b3f14a575f36d5d5ed3
Han Wang <416810799@qq.com>
2018-10-02 05:17:10 +0000

    kuntao: Update textclassifier package name for P
    Change-Id: If846cc590060025202fa336af4420747f6b01238
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- device.mk
+++ device.mk
@@ -144,7 +144,7 @@ PRODUCT_PACKAGES += \
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
-    textclassifier.smartselection.bundle1
+    textclassifier.bundle1
 
 # DRM
 PRODUCT_PACKAGES += \
