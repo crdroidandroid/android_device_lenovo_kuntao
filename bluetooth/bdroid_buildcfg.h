16322844e133b3b7e74ea728583c7ecb5ce3000a
Murat Kozan <muratkozan350@gmail.com>
2018-10-30 18:06:42 +0300

    kuntao: bluetooth: Set proper model name
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Lenovo P2"
+#define BTM_DEF_LOCAL_NAME   "Lenovo P2a42"
 #define BLUETOOTH_QTI_SW TRUE
 
 // Disables read remote device feature
